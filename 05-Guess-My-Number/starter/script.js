c0a1edef20029f72d6c3575c4b8df5d0a92e90cd
Sahal2000 <me.sahal2000@gmail.com>
2024-10-01 17:33:09 +0600

    Attributes, Classes and IDs

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,8 +2,8 @@
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
 
-document.querySelector('.number') = 13;
+document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
-console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
