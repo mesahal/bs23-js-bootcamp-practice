d745e8d465a299c35ea0e4cce45967ecfaab7ca4
Sahal2000 <me.sahal2000@gmail.com>
2024-10-02 20:03:32 +0600

    reset button

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,9 +4,19 @@ document.querySelector('.message').textContent = 'Correct Number!';
 
 let score = 20;
 let highScore = 0;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.score').textContent = score;
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  highScore = 0;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.highscore').textContent = highScore;
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -15,6 +25,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct  number';
+    document.querySelector('.number').textContent = secretNumber;
     highScore = Math.max(highScore, score);
     console.log(score, highScore);
     document.querySelector('.highscore').textContent = highScore;
